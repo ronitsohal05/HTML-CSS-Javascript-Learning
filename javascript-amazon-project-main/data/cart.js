d013eb65f993362a73798ca0a86a1629c794ad48
Ronit <ronitsohal@gmail.com>
2025-01-03 17:19:33 -0500

    Moved addToCart function to cart.js in amazon project

--- javascript-amazon-project-main/data/cart.js
+++ javascript-amazon-project-main/data/cart.js
@@ -1,1 +1,20 @@
 export const cart = [];
+
+export function addToCart(productId){
+    let matchingItem;
+
+    cart.forEach((cartItem) => {
+        if(productId === cartItem.productId){
+            matchingItem = cartItem;
+        }
+    });
+
+    if(matchingItem){
+        matchingItem.quantitiy+=1;
+    }else{
+        cart.push({
+            productId: productId,
+            quantitiy: 1
+        });
+    }
+}
