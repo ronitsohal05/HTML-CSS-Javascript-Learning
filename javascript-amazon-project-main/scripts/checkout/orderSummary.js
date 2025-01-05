2931f14f10d938c4ce452fa07234aad72113748f
Ronit <ronitsohal@gmail.com>
2025-01-05 14:36:50 -0500

    Make payment summary interactive

--- javascript-amazon-project-main/scripts/checkout/orderSummary.js
+++ javascript-amazon-project-main/scripts/checkout/orderSummary.js
@@ -3,12 +3,7 @@ import { products, getProduct} from '../../data/products.js';
 import { formatCurrency } from '../utils/money.js';
 import dayjs from 'https://unpkg.com/dayjs@1.11.10/esm/index.js';
 import {deliveryOptions, getDeliveryOption} from '../../data/deliveryOptions.js';
-
-
-
-const today = dayjs();
-const deliveryDate = today.add(7, 'days');
-deliveryDate.format('dddd, MMMM D')
+import { renderPaymentSummary } from './paymentSummary.js';
 
 
 export function renderOrderSummary(){
@@ -22,7 +17,7 @@ export function renderOrderSummary(){
       const matchingProduct = getProduct(productId);
 
       const deliveryOptionId = cartItem.deliveryOptionsId;
-      
+
       let deliveryOption = getDeliveryOption(deliverOptionId);
 
       const today = dayjs();
@@ -129,11 +124,14 @@ export function renderOrderSummary(){
         const productId = link.dataset.productId;
         removeFromCart(productId);
 
+        
 
         const container = document.querySelector(
           `.js-cart-item-container-${productId}`
         );
         container.remove();
+
+        renderPaymentSummary();
       });
     });
 
@@ -143,6 +141,7 @@ export function renderOrderSummary(){
         const {productId, deliveryOptionId} = element.dataset;
         updateDeliveryOption(productId,deliveryOptionId);
         renderOrderSummary();
+        renderPaymentSummary();
       });
     });
 }
