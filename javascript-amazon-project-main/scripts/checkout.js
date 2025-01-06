e3b329888cd039154af1e4d74d57ba03251b15ab
Ronit <ronitsohal@gmail.com>
2025-01-06 16:40:34 -0500

    learn promises and coverted to code to promises (Amazon project)

--- javascript-amazon-project-main/scripts/checkout.js
+++ javascript-amazon-project-main/scripts/checkout.js
@@ -1,10 +1,50 @@
 import {renderOrderSummary} from './checkout/orderSummary.js';
 import {renderPaymentSummary} from './checkout/paymentSummary.js';
 import { loadProducts } from '../data/products.js';
+import {loadCart} from '../data/cart.js'
 // import '../data/cart-class.js';
-import '../data/backend-practice.js'
+//import '../data/backend-practice.js'
+Promise.all([
+    new Promise((resolve) => {
+        loadProducts(() => {
+            resolve();
+        });
+    
+    }),
+    new Promise((resolve) => {
+        loadCart(() => {
+            resolve();
+        });
+    })
 
-loadProducts(() =>{
+]).then(() => {
     renderOrderSummary();
     renderPaymentSummary();
 });
+
+/*new Promise((resolve) => {
+    loadProducts(() => {
+        resolve();
+    });
+
+}).then(() => {
+    return new Promise((resolve) => {
+        loadCart(() => {
+            resolve();
+        });
+    });
+
+}).then(()=>{
+    renderOrderSummary();
+    renderPaymentSummary();
+});*/
+
+
+/*
+loadProducts(() =>{\
+    loadCart(() => {
+        renderOrderSummary();
+        renderPaymentSummary();
+    });
+});
+*/
